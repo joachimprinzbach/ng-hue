593d99cec9f98b153a154b0cebbf1a83ef10d716
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2017-01-07 22:02:39 +0100

    print out lights for bridge

--- src/app/hue/hue.service.ts
+++ src/app/hue/hue.service.ts
@@ -5,18 +5,19 @@ import {Observable} from "rxjs";
 @Injectable()
 export class HueService {
 
-  constructor(private http: Http) {
-  }
+    constructor(private http: Http) {
+    }
 
-  findBridges() {
-    return this.http
-      .get('https://www.meethue.com/api/nupnp')
-      .map(response => response.json());
-  }
+    findBridges() {
+        return this.http
+            .get('https://www.meethue.com/api/nupnp')
+            .map(response => response.json());
+    }
 
-  loadLights(bridgeIp: string) {
-    return this.http
-      .get('http://' + bridgeIp + '/api/WBgiileJ7lyApsgJ3OJxdH7Zd6Ioe9VlxfODLgVa/lights')
-      .map(response => response.json());
-  }
+    loadLights(bridgeIp: string) {
+        return this.http
+            .get('http://' + bridgeIp + '/api/WBgiileJ7lyApsgJ3OJxdH7Zd6Ioe9VlxfODLgVa/lights')
+            .map(response => response.json())
+            .map(resObj => Object.keys(resObj).map(key => resObj[key]));
+    }
 }
